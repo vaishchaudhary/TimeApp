9bd30ecdb292715a337911b97cfcb7fda4e8625d
vaishali chaudhary <vaishali.chaudhary@unthinkable.co>
2020-09-08 11:10:27 +0530

    third comment

--- lib/main.dart
+++ lib/main.dart
@@ -9,22 +9,19 @@ import 'package:location/location.dart';
 
 import 'WeatherModel.dart';
 
-void main() {
-  runApp(MyApp());
-}
-
 Future<WeatherModel> getWeather(String lat, String lng) async {
   final response = await http.get(
-      'http://api.openweathermap.org/data/2.5/weather?lat=$lat&lon=$lng&appid=51663d3c484f2d7d48d8a3ab31715333&units=metric');
+      'https://api.openweathermap.org/data/2.5/weather?lat=$lat&lon=$lng&appid=51663d3c484f2d7d48d8a3ab31715333&units=metric');
 
   if (response.statusCode == 200) {
     var result = json.decode(response.body);
-    print(response.body);
     var model = WeatherModel.fromJson(result);
     return model;
   } else
     throw Exception('Failed to load Weather Information');
 }
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
   @override
@@ -33,8 +30,7 @@ class MyApp extends StatefulWidget {
 
 class MyAppState extends State<MyApp> {
   Map<String, double> currentLocation = new Map();
-
-//  StreamSubscription<Map<String, double>> locationSubscription;
+  StreamSubscription<LocationData> locationSubscription;
 
   Location location = new Location();
   String error;
@@ -43,24 +39,24 @@ class MyAppState extends State<MyApp> {
   void initState() {
     super.initState();
     //Default value
-    currentLocation['latitude'] = 25.6667;
-    currentLocation['longitude'] = -100.3167;
+    currentLocation['latitude'] = 0.0;
+    currentLocation['longitude'] = 0.0;
 
-    // initPlatformState();
+    initPlatformState();
 
-//    locationSubscription =
-//        location.onLocationChanged().listen((Map<String, double> result) {
-//          setState(() {
-//            currentLocation = result;
-//          });
-//        });
+    locationSubscription =
+        location.onLocationChanged.listen((LocationData result) {
+      setState(() {
+        currentLocation['latitude'] = result.latitude;
+        currentLocation['longitude'] = result.longitude;
+      });
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Weather App',
-      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         brightness: Brightness.light,
         primaryColor: Color.fromRGBO(61, 90, 254, 1.0),
@@ -71,7 +67,7 @@ class MyAppState extends State<MyApp> {
       home: Scaffold(
         appBar: AppBar(
           title: Text(
-            'Weather Information',
+            'Weather App',
             style: TextStyle(
               fontWeight: FontWeight.bold,
               color: Colors.white,
@@ -84,17 +80,16 @@ class MyAppState extends State<MyApp> {
         body: SingleChildScrollView(
           child: Container(
             decoration: new BoxDecoration(
-              color: Color.fromRGBO(61, 90, 254, 1.0),
-//                gradient: LinearGradient(
-//                  begin: Alignment.topCenter,
-//                  end: Alignment.bottomCenter,
-//                  colors: [
-//                    // Colors for gradients
-//                    Color.fromRGBO(31, 42, 255, 1.0),
-//                    Color.fromRGBO(230, 60, 167, 1.0),
-//                  ],
-//                )
-            ),
+                color: Color.fromRGBO(61, 90, 254, 1.0),
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [
+                    // Colors for gradients
+                    Color.fromRGBO(31, 42, 255, 1.0),
+                    Color.fromRGBO(230, 60, 167, 1.0),
+                  ],
+                )),
             child: FutureBuilder<WeatherModel>(
               future: getWeather(currentLocation['latitude'].toString(),
                   currentLocation['longitude'].toString()),
@@ -125,11 +120,6 @@ class MyAppState extends State<MyApp> {
                             crossAxisAlignment: CrossAxisAlignment.center,
                             mainAxisAlignment: MainAxisAlignment.center,
                             children: <Widget>[
-                              //Image.network(
-                              //  'https://openweathermap.org/img/w/${model.weather[0].icon}.png',
-                              //  width: 50,
-                              //  height: 50,
-                              //),
                               Image.asset(
                                 'assets/images/weather/${model.weather[0].icon}.png',
                                 width: 100,
@@ -544,7 +534,61 @@ class MyAppState extends State<MyApp> {
                       ),
                     ],
                   );
-                }
+                } else if (snapshot.hasError)
+                  return Container(
+                    height: 500,
+                    margin: EdgeInsets.all(10),
+                    padding: EdgeInsets.all(10),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Image.asset(
+                          'assets/images/bug_fixed.png',
+                          width: 250,
+                          fit: BoxFit.fitWidth,
+                        ),
+                        Text(
+                          ' ',
+                          style: TextStyle(
+                            height: 1.5,
+                          ),
+                        ),
+                        Text(
+                          'Oh Oh, Houston we have a problem!',
+                          style: TextStyle(
+                              fontSize: 20.0,
+                              height: 1.0,
+                              letterSpacing: 0.25,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white),
+                        ),
+                        Text(
+                          ' ',
+                          style: TextStyle(
+                            height: 1.0,
+                          ),
+                        ),
+                        Text(
+                          'Check your network or give the location permission to the app.',
+                          style: TextStyle(
+                              fontSize: 15.0,
+                              height: 1.0,
+                              letterSpacing: 0.25,
+                              fontWeight: FontWeight.normal,
+                              color: Colors.white),
+                        ),
+                        //Text(
+                        //  '${snapshot.error}',
+                        //  style: TextStyle(
+                        //      fontSize: 20.0,
+                        //      height: 1.5,
+                        //      letterSpacing: 0.25,
+                        //      color: Colors.red),
+                        //),
+                      ],
+                    ),
+                  );
                 return CircularProgressIndicator(); //Default show loading
               },
             ),
@@ -569,14 +613,6 @@ class MyAppState extends State<MyApp> {
                   onPressed: () {},
                 ),
               ),
-              Expanded(child: new Text('')),
-              Expanded(
-                child: IconButton(
-                  icon: Icon(Icons.search),
-                  color: Color.fromRGBO(61, 90, 254, 1.0),
-                  onPressed: () {},
-                ),
-              ),
             ],
           ),
         ),
@@ -585,21 +621,23 @@ class MyAppState extends State<MyApp> {
     );
   }
 
-//  void initPlatformState() async {
-//    Map<String, double> my_location;
-//    try {
-//      my_location = await location.getLocation();
-//      error = '';
-//    } on PlatformException catch (e) {
-//      if (e.code == 'PERMISSION_DENIED')
-//        error = 'Permission Denied';
-//      else if (e.code == 'PERMISSION_DENIED_NEVER_ASK')
-//        error =
-//        'Permission Denied - Please ask the user to enable it from app settings';
-//      my_location = null;
-//    }
-//    setState(() {
-//      currentLocation = my_location;
-//    });
-//  }
+  void initPlatformState() async {
+    LocationData myLocation;
+    try {
+      myLocation = await location.getLocation();
+      error = '';
+    } on PlatformException catch (e) {
+      if (e.code == 'PERMISSION_DENIED')
+        error = 'Permission Denied';
+      else if (e.code == 'PERMISSION_DENIED_NEVER_ASK')
+        error =
+            'Permission Denied - Please ask the user to enable it from app settings';
+      myLocation = null;
+    }
+    setState(() {
+      currentLocation['latitude'] = myLocation.latitude;
+      currentLocation['longitude'] = myLocation.longitude;
+      print(myLocation.toString());
+    });
+  }
 }
